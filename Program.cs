b49b73ac0548f8e44753452e7ee59fc39c0f9003
Minkah <kyikandy@gmail.com>
2025-01-16 21:39:34 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int length = 10;
+            int width = 5;
+            int area = length * width;
+            Console.WriteLine(area);
+        }
+    }
+}
